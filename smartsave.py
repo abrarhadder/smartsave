053c633c3f668b6f7be6c62d4dece5ba9bab35c6
abrarhadder <79809868+abrarhadder@users.noreply.github.com>
2021-03-30 17:43:39 -0500

    Update smartsave.py
    creating a browse folder function

--- smartsave.py
+++ smartsave.py
@@ -69,3 +69,13 @@ class SmartSaveUI(QtWidgets.QDialog):
         self.scenefile.task = self.task_le.text()
         self.scenefile.ver = self.ver_sbx.value()
         self.scenefile.ext = self.ext_lbl.text()
+
+        @QtCore.Slot()
+        def _browse_folder(self):
+            """opens a dialogue box to browse the folder"""
+            folder = QtWidgets.QFileDialog.getExistingDirectory(
+                parent=self, caption="Select Folder",
+                dir=self.folder_le.text(),
+                options=QtWidgets.QFileDialog.ShowDirsOnly |
+                        QtWidgets.QFileDialog.DontResolveSymlinks)
+            self.folder_le.setText(folder)
