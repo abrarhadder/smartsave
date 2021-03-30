c3fa36f8682caf38300f7d9249964630555e47b4
abrarhadder <79809868+abrarhadder@users.noreply.github.com>
2021-03-30 17:50:59 -0500

    Update smartsave.py
    reformatting the code and reimplementing scene file class

--- smartsave.py
+++ smartsave.py
@@ -10,14 +10,15 @@ from pymel.core.system import Path
 
 log = logging.getLogger(__name__)
 
+
 def maya_main_window():
     """Return the maya main window widget"""
     main_window = omui.MQtUtil.mainWindow()
     return wrapInstance(long(main_window), QtWidgets.QWidget)
 
+
 class SmartSaveUI(QtWidgets.QDialog):
     """Smart Class UI Class"""
-
 
     def __init__(self):
         super(SmartSaveUI, self).__init__(parent=maya_main_window())
@@ -131,3 +132,89 @@ class SmartSaveUI(QtWidgets.QDialog):
                 layout.addWidget(self.folder_le)
                 layout.addWidget(self.folder_browse_btn)
                 return layout
+
+
+class SceneFile(object):
+    """An abstract representation of a Scene file."""
+
+    def __init__(self, path=None):
+        self._folder_path = Path(os.getcwd()) / "scenes"
+        self.descriptor = 'main'
+        self.task = 'model'
+        self.ver = 1
+        self.ext = '.ma'
+        scene = pmc.system.sceneName()
+        if not path and scene:
+            path = scene
+        if not path and not scene:
+            log.info("Initialize with default properties.")
+            return
+        self._init_from_path(path)
+
+    @property
+    def folder_path(self):
+        return self._folder_path
+
+    @folder_path.setter
+    def folder_path(self, val):
+        self._folder_path = Path(val)
+
+    @property
+    def filename(self):
+        pattern = "{descriptor}_{task}_v{ver:03d}.{ext}"
+        return pattern.format(descriptor=self.descriptor,
+                              task=self.task,
+                              ver=self.ver,
+                              ext=self.ext)
+
+    @property
+    def path(self):
+        return self.folder_path / self.filename
+
+    def _init_from_path(self, path):
+        path = Path(path)
+        self.folder_path = path.parent
+        self.ext = path.ext
+        self.descriptor, self.task, ver = path.name.stripext().split("_")
+        self.ver = int(ver.split("v")[-1])
+
+    def save(self):
+        """saves the scene file.
+
+        returns:
+            path: the file to the scene file if successful
+        """
+        try:
+            return pmc.system.saveAs(self.path)
+        except RuntimeError as err:
+            log.warning("Missing directories in path. Creating directories...")
+            self.folder_path.makedirs_p()
+            return pmc.system.saveAs(self.path)
+
+    def next_avail_ver(self):
+        """Return the next available version number in this folder."""
+        pattern = "{descriptor}_{task}_v*{ext}".format(
+            descriptor=self.descriptor, task=self.task, ext=self.ext)
+        matching_scenefiles = []
+        for file_ in self.folder_path.files():
+            if file_.name.fnmatch(pattern):
+                matching_scenefiles.append(file_)
+        if not matching_scenefiles:
+            return 1
+        matching_scenefiles.sort(reverse=True)
+        latest_scenefile = matching_scenefiles[0]
+        latest_scenefile = latest_scenefile.name.stripext()
+        latest_ver_num = int(latest_scenefile.split("v")[-1])
+        return latest_ver_num + 1
+
+    def save_increment(self):
+        """Increments the version and saves the scene file.
+
+        If the existing version of a file already exists, it should increment
+        from the largest version number available in the folder.
+
+        Returns:
+            Path: the path to the scene file if successful
+        """
+        self.ver = self.next_avail_ver()
+        self.save()
