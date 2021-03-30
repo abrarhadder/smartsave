fdf51f7fc3345f4734b6b7ecf15a03da94cff536
abrarhadder <79809868+abrarhadder@users.noreply.github.com>
2021-03-30 17:33:32 -0500

    Create smartsave.py
    creating maya main window

---
+++ smartsave.py
@@ -0,0 +1,16 @@
+import logging
+
+from PySide2 import QtWidgets, QtCore
+from shiboken2 import wrapInstance
+import maya.OpenMayaUI as omui
+import maya.cmds as cmds
+import pymel.core as pmc
+import os
+from pymel.core.system import Path
+
+log = logging.getLogger(__name__)
+
+def maya_main_window():
+    """Return the maya main window widget"""
+    main_window = omui.MQtUtil.mainWindow()
+    return wrapInstance(long(main_window), QtWidgets.QWidget)
