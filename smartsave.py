bc31c77779b4992b7362464ffbfce8cf19a9d97f
abrarhadder <79809868+abrarhadder@users.noreply.github.com>
2021-03-30 17:36:39 -0500

    Update smartsave.py
    creating and initializing smart save UI class and connection and UI functions.

--- smartsave.py
+++ smartsave.py
@@ -14,3 +14,38 @@ def maya_main_window():
     """Return the maya main window widget"""
     main_window = omui.MQtUtil.mainWindow()
     return wrapInstance(long(main_window), QtWidgets.QWidget)
+
+class SmartSaveUI(QtWidgets.QDialog):
+    """Smart Class UI Class"""
+
+
+    def __init__(self):
+        super(SmartSaveUI, self).__init__(parent=maya_main_window())
+        self.setWindowTitle("Smart Save")
+        self.setMinimumWidth(500)
+        self.setMaximumHeight(200)
+        self.setWindowFlags(self.windowFlags() ^
+                            QtCore.Qt.WindowContextHelpButtonHint)
+        self.scenefile = SceneFile()
+        self.create_ui()
+        self.create_connections()
+
+    def create_ui(self):
+        self.title_lbl = QtWidgets.QLabel("Smart Save")
+        self.title_lbl.setStyleSheet("font: bold 20px")
+        self.folder_lay = self._create_folder_ui()
+        self.filename_lay = self._create_filename_ui()
+        self.button_lay = self._create_buttons_ui()
+        self.main_lay = QtWidgets.QVBoxLayout()
+        self.main_lay.addWidget(self.title_lbl)
+        self.main_lay.addLayout(self.folder_lay)
+        self.main_lay.addLayout(self.filename_lay)
+        self.main_lay.addStretch()
+        self.main_lay.addLayout(self.button_lay)
+        self.setLayout(self.main_lay)
+
+    def create_connections(self):
+        """connect signals and slots"""
+        self.folder_browse_btn.clicked.connect(self._browse_folder)
+        self.save_btn.clicked.connect(self._save)
+        self.save_inc_btn.clicked.connect(self._save_increment)
